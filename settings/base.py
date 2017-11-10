cac27442c4841c769b27778f0b975758645e752b
Toni Colom <toni@semicolom.com>
2017-11-10 17:02:31 +0100

    Added django-extensions

--- settings/base.py
+++ settings/base.py
@@ -28,14 +28,17 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
