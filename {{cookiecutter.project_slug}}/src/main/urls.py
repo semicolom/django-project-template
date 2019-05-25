481e8128d80208e5b93a1ee4650a5f2d4229407f
Toni Colom <toni@semicolom.com>
2019-05-25 10:00:04 +0200

    Disables debug toolbar by default and fixes admin url

--- {{cookiecutter.project_slug}}/src/main/urls.py
+++ {{cookiecutter.project_slug}}/src/main/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('{}'.format(settings.ADMIN_URL), admin.site.urls),
+    path('{}/'.format(settings.ADMIN_URL), admin.site.urls),
 ]
 
 # On development serve media and static files using django
